6ff828df5e1d276ed8179830ea43c3559ac234f4
ShMelL25 <108915857+ShMelL25@users.noreply.github.com>
2022-07-29 00:25:22 +0300

    Delete view_prog.py
    Не нужен

--- view_prog.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QAction, qApp, QHBoxLayout, QVBoxLayout, QPushButton)
-from PyQt5.QtGui import QIcon
-
-class view_menuBar(QMainWindow, QWidget):
-    def exitaction(self):
-        exitAction = QAction(QIcon('img\icons8-выход-48.png'), '&Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
