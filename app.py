0d8d887cecdec62120b16c23ed3c432d4f6f3954
Egor Shchemelyov <egor2014ag@gmail.com>
2022-07-29 12:33:23 +0300

    Создание кнопки new_file

--- app.py
+++ app.py
@@ -10,24 +10,33 @@ class Example(QMainWindow, QWidget):
         super().__init__()
         self.initUI()
         self.exitaction()
+        self.status_Bar()
     
+    def initUI(self):
+        self.setGeometry(300, 300, 1000, 500)
+        self.showMaximized()
+        self.setWindowTitle('Исследования')
+        self.setWindowIcon(QIcon('img\icons8-научное-приложение-50.png')) # <a target="_blank" href="https://icons8.com/icon/40391/научное-приложение">Научное приложение</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>   
+        self.show()
+
     def exitaction(self):
-        exitAction = QAction(QIcon('img\icons8-выход-48.png'), '&Exit', self)
+        new_file = QAction(QIcon('img\icons8-создать-новый-48.png'), '&Новый документ', self)
+        new_file.setShortcut('Ctrl+N')
+        new_file.setStatusTip('Создание нового документа')
+        new_file.trigger
+
+        exitAction = QAction(QIcon('img\icons8-выход-48.png'), '&Выход', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
         menubar = self.menuBar()
         
         fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAction)    
+        fileMenu.addAction(new_file)
+        fileMenu.addAction(exitAction)
 
-    def initUI(self):
+    def status_Bar(self):
         self.statusBar().showMessage('Ready')
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Исследования')
-        self.setWindowIcon(QIcon('img\icons8-научное-приложение-50.png')) # <a target="_blank" href="https://icons8.com/icon/40391/научное-приложение">Научное приложение</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-                
-        self.show()
 
 def main():
     app = QApplication(sys.argv)
