5c5ba2f78f7b332f7a493b385021c184f0aa852a
Egor Shchemelyov <egor2014ag@gmail.com>
2022-08-06 23:13:04 +0300

    BUGFIX

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from email.mime import application
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QAction, qApp, QHBoxLayout, QVBoxLayout, QPushButton)
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon
 from mainwidgets import Ui_MainWindow
 
 class mywindow(QtWidgets.QMainWindow):
@@ -10,6 +11,8 @@ class mywindow(QtWidgets.QMainWindow):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+    
 
 def main():
     app = QtWidgets.QApplication([])
