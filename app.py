135204c808d473122d9384bfa18ebaeb9e96b798
Egor Shchemelyov <egor2014ag@gmail.com>
2022-08-06 18:27:52 +0300

    Создан примерный вид приложения

--- app.py
+++ app.py
@@ -1,48 +1,21 @@
+from email.mime import application
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QAction, qApp, QHBoxLayout, QVBoxLayout, QPushButton)
-from PyQt5.QtGui import QIcon
-from numpy import save
+from PyQt5 import QtCore, QtGui, QtWidgets
+from mainwidgets import Ui_MainWindow
 
-
-
-class Example(QMainWindow, QWidget):
+class mywindow(QtWidgets.QMainWindow):
     
     def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.exitaction()
-        self.status_Bar()
-    
-    def initUI(self):
-        self.setGeometry(300, 300, 1000, 500)
-        self.showMaximized()
-        self.setWindowTitle('Исследования')
-        self.setWindowIcon(QIcon('img\icons8-научное-приложение-50.png')) # <a target="_blank" href="https://icons8.com/icon/40391/научное-приложение">Научное приложение</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>   
-        self.show()
-
-    def exitaction(self):
-        save_file = QAction(QIcon('img\icons8-создать-новый-48.png'), '&Новый документ', self)
-        save_file.setShortcut('Ctrl+N')
-        save_file.setStatusTip('Создание нового документа')
-        save_file.trigger
-        menubar = self.menuBar()
-
-        exitAction = QAction(QIcon('img\icons8-выход-48.png'), '&Выход', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
-        menubar = self.menuBar()
-        
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(save_file)
-        fileMenu.addAction(exitAction)
-
-    def status_Bar(self):
-        self.statusBar().showMessage('Ready')
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 def main():
-    app = QApplication(sys.argv)
-    ex = Example()
+    app = QtWidgets.QApplication([])
+    application = mywindow()
+    application.show()
+
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
