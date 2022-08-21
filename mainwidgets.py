daedcc4c380bdd2c39e1b1ddcfb23621378c0434
ShMelL25 <egor2014ag@mail.ru>
2022-08-21 21:09:18 +0300

    Fix

--- mainwidgets.py
+++ mainwidgets.py
@@ -54,34 +54,27 @@ class Ui_MainWindow(Sorrymywindow, object):
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
         self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 218, 440))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.gridLayout = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
-        self.gridLayout.setObjectName("gridLayout")
-        self.gridLayout.setSpacing(1)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout.setObjectName("QVBoxLayout")
+        self.verticalLayout.setSpacing(10)
         self.scrollArea_Name.setWidget(self.scrollAreaWidgetContents)
         self.horizontalLayout_2.addWidget(self.scrollArea_Name)
 
         # Добавление надписи Испытуемые
         self.isp_label = QtWidgets.QLabel(self.scrollArea_Name)
         self.isp_label.setText('Испытуемые')
-        self.gridLayout.addWidget(self.isp_label, 0, 1)
-
-        self.isp_label_1 = QtWidgets.QLabel(self.scrollArea_Name)
-        self.isp_label_1.setText('1234')
-        self.gridLayout.addWidget(self.isp_label_1, 1, 1)
-
-        self.isp_label_2 = QtWidgets.QLabel(self.scrollArea_Name)
-        self.isp_label_2.setText('12345')
-        self.gridLayout.addWidget(self.isp_label_2, 1, 2)
         
-       
-
+        
+        
+        
+            
         self.scrollArea_Research = QtWidgets.QScrollArea(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(20)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.scrollArea_Research.sizePolicy().hasHeightForWidth())
         self.scrollArea_Research.setSizePolicy(sizePolicy)
-        self.scrollArea_Research.setWidgetResizable(True)
+        self.scrollArea_Research.setWidgetResizable(False)
         self.scrollArea_Research.setObjectName("scrollArea_Research")
         self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
         self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 548, 440))
